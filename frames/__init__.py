20c69972d3894ecf4873e455cd5d2b15886122ab
Prasanna Palanivel <129665508+Unmask06@users.noreply.github.com>
2025-01-16 17:19:03 +0400

    Add multi-case workflow frame

--- frames/__init__.py
+++ frames/__init__.py
@@ -3,3 +3,4 @@ from .create_model import init_create_model_frame
 from .update_condition import init_update_conditions_frame
 from .run_simulation import init_run_simulation_frame
 from .summarize import init_summarize_frame
+from .multi_case import init_multi_case_frame
